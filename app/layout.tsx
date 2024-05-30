70a26c7a8cfa56f205839b564227b843e9746553
leafartech <119262053+leafartech@users.noreply.github.com>
2024-05-30 13:51:59 -0300

    gm changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,11 +43,21 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MX3KQ9G')
+        `}} />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-NLM24BX')
         `}} />
       </head>
       <body className={`${sourceserif.variable} ${montserrat.variable}`}>
         <SpeedInsights />
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX3KQ9G"
+          height="0" width="0"></iframe></noscript>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLM24BX"
           height="0" width="0"></iframe></noscript>
         {children}
