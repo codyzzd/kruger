67d5f4c227d90f2befd782b0f77ef9c46a343408
Rafael Bezerra <119262053+leafartech@users.noreply.github.com>
2024-11-19 12:54:38 -0300

    Delete app directory

--- app/sections/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <section className="px-8 py-8 bg-slate-100">
-      <footer className="container mx-auto text-center ">
-        <p>KRÜGER TOLEDO © Todos os direitos reservados.</p>
-      </footer>
-    </section>
-  );
-}
